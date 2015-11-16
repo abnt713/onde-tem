8dfa76b6176fa9c1a8f8c874773e4758c5bc7c43
souzabrizolara <souzabrizolara@gmail.com>
2015-11-16 16:50:22 -0300

    Mudanças em views para tornar mapa fullscreen

--- ondetem-client/app/components/ondetem-map.js
+++ ondetem-client/app/components/ondetem-map.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['fullheight'],
+
 	markers : [],
 	mapHeight: '200px',
 	center : [51.5, -0.09],
@@ -10,26 +12,43 @@ export default Ember.Component.extend({
 			iconRetinaUrl: 'assets/images/marker-icon@2x.png',
 			shadowUrl: 'assets/images/marker-shadow.png'
 	}),
+	
 	didInsertElement : function(){
+			this.buildMap();
+	},
+
+	buildMap :function(){
+			var map = this._Map();
+
+			this._addMapLayer(map);
+
+			var markers = this.get('markers');
+			this._addMarkers(map, markers);
+	},
+
+	_Map : function(){
 			var center = this.get('center');
 			var zoom = this.get('zoom');
 
 			var mapElem = this.$('.map').get(0);
 			var map = L.map(mapElem).setView(center, zoom);
 
+			return map;
+	},
+	_addMapLayer : function(map){
 			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map);
-
-			var markers = this.get('markers');
-			if(markers){
-					for(var i=0; i < markers.length; ++i){
-							var marker = markers[i];
-							L.marker([marker.x, marker.y], {icon: this.getIcon()})
-								.addTo(map)
-							  .bindPopup(String(marker.label) + "[" + marker.x + ", " + marker.y + "]").openPopup();
-					}
-			}
+	},
+	_addMarkers : function(map, markers){
+		if(markers){
+				for(var i=0; i < markers.length; ++i){
+						var marker = markers[i];
+						L.marker([marker.x, marker.y], {icon: this.getIcon()})
+							.addTo(map)
+							.bindPopup(String(marker.label) + "[" + marker.x + ", " + marker.y + "]").openPopup();
+				}
+		}
 	},
 
 	getIcon : function(){
